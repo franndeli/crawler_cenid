9c10b09e835112ccae5b28c48b2ffe5995057db3
marcos <woozymarcos@gmail.com>
2024-03-14 14:19:12 +0100

    Herramienta cleatext alpha

--- crawlers/crawler_marcos/cleantext.js
+++ crawlers/crawler_marcos/cleantext.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // Leer el archivo JSON
-fs.readFile('documento.json', 'utf8', (err, data) => {
+fs.readFile('crawlers\\crawler_marcos\\Estatutos de Plena inclusión Castilla y León(LF).json', 'utf8', (err, data) => {
     if (err) {
         console.error('Error al leer el archivo:', err);
         return;
@@ -13,16 +13,22 @@ fs.readFile('documento.json', 'utf8', (err, data) => {
 
         // Función para limpiar el texto
         function limpiarTexto(documento) {
-            // Expresión regular para eliminar caracteres no deseados
-            const regex = /[^\w\sáéíóúüñ.,;:!¡¿?()]/g;
 
             // Filtrar y limpiar cada página de texto
             const textoLimpiado = documento.document.map(page => {
+
                 // Eliminar caracteres no deseados
-                let cleanContent = page.content.replace(regex, '');
+                let cleanContent = page.content.replace(//g, '');
 
                 // Eliminar espacios y saltos de línea adicionales
-                cleanContent = cleanContent.trim().replace(/\n+/g, '\n');
+                cleanContent = cleanContent.trim().replace(/\n+/g, ' ');
+                cleanContent = cleanContent.trim().replace(/ +/g, ' ');
+                cleanContent = cleanContent.trim().replace(/^[0-9][0-9] Estatutos Sociales de Plena inclusión en Lectura Fácil /g, '');
+                cleanContent = cleanContent.trim().replace(/^[0-9][0-9] /g, '');
+                cleanContent = cleanContent.trim().replace(/^[0-9] /g, '');
+                cleanContent = cleanContent.trim().replace(/ ./g, '.');
+
+
 
                 // Asignar el contenido limpiado a la página
                 page.content = cleanContent;
@@ -43,7 +49,7 @@ fs.readFile('documento.json', 'utf8', (err, data) => {
         const documentoLimpiadoJSON = JSON.stringify(documentoLimpiado, null, 2);
 
         // Escribir el documento limpiado de nuevo en un archivo JSON
-        fs.writeFileSync('documento_limpiado.json', documentoLimpiadoJSON, 'utf8');
+        fs.writeFileSync('crawlers\\crawler_marcos\\documento_limpiado.json', documentoLimpiadoJSON, 'utf8');
 
         console.log('El archivo documento_limpiado.json se ha creado con éxito.');
     } catch (error) {
